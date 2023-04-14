459194b833e9d5880d965110a8e87afb54e4c90a
Learn Today <learntodayfromai@gmail.com>
2023-04-14 13:54:23 -0700

    material design

--- app/src/main/java/com/learntodayfromai/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/learntodayfromai/tipcalculator/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculateTip() {
-        val stringInTextField = binding.costOfService.text.toString()
+        val stringInTextField = binding.costOfService.editText.toString()
         val cost = stringInTextField.toDoubleOrNull()
         if (cost ==null){
             binding.tipResult.text=""
@@ -41,21 +41,4 @@ class MainActivity : AppCompatActivity() {
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 
-    fun calculateTip2() {
-        val input = binding.costOfService.text.toString()
-        val cost = input.toDouble()
-        val selected = binding.tipOptions.checkedRadioButtonId
-        val tipPercent = when(selected){
-            R.id.option_twenty_percent -> 0.20
-            R.id.option_eighteen_percent -> 0.18
-            else -> 0.15
-        }
-        var tip = tipPercent*cost
-        val roundUp = binding.roundUpSwitch.isChecked
-        if (roundUp){
-            tip = kotlin.math.ceil(tip)
-        }
-        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-        binding.tipResult.text = getString(R.string.tip_amount,formattedTip)
-    }
 }
